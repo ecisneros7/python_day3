47f26372fbd4832c9cd4b4ffbe8d4856802edd1e
Cisneros <ecisneros7@cps.k12.il.us>
2024-08-30 15:06:30 -0500

    first commit

---
+++ lesson2.py
@@ -0,0 +1,52 @@
+#create a print function that prints "Hello, World!"
+#out the string "Hello, World!"
+# print("Hello, World!")
+# ask the user for the day of the week
+# day_of_week = input("What day of the week is it? ")
+# print("Today is"+ day_of_week)
+# concatenation is when you add two strings together
+# using a plus sign (+)
+# movies_this_week = input("what movies are you watching this week?")
+# print("I am watching"+ movies_this_week + "this week")
+# mood = input("How are you feeling today?")
+# print("I am feeling"+ mood )
+# data types for variables in python
+# strings - text
+name = "John" # this is a string data type
+#whenever it is wrapped in quottes it is a string
+year = "2024"
+# integers - whole numbers
+year = 2024 # this is an integer data type
+# does not need to be wrapped in quotes
+yearFourFromNow = 2028
+subtract = yearFourFromNow - year
+print(subtract)
+# floats - decimal numbers
+priceBigMac = 3.99 # this is a float data type
+priceDoublePounder = 4.99
+totalPrice = priceBigMac + priceDoublePounder
+print(totalPrice)
+# booleans - True or False
+isRaining = False # this is a boolean data type
+print(isRaining)
+# lists - a collection of items
+groceries = ["apples", "bananas", "carrots"]
+print(groceries)
+movieLocation = input("Where is the movie theater?")
+moviePrice = input("How much do the tickets cost?")
+print(movieList)
+movieName = "Twisters"
+movieTime = "8:00pm"
+movieprice = 40.65
+dinnerTime = "6:00 PM"
+dinnerPrice = 15.99
+restaurantAddress = "1234 Main Street"
+inEvening = True
+peopleGoing = input("How many people are going?")
+#type conversion converting from one data type to another
+#print the entire output in one sentence
+print("We are going to see" + movieName +
+      "at"+ movieTime +
+      "and then eat dinner at"+
+      restaurantAddress + "at"+
+      dinnerTime + "with"+ peopleGoing + "people")
